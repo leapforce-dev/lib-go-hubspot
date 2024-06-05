ec9bfc0674f9c7428344f7d9876c4548d9627c39
caselongo <caselongo@gmail.com>
2024-06-05 22:26:39 +0200

    added Webhook.go

---
+++ Webhook.go
@@ -0,0 +1,16 @@
+package hubspot
+
+type WebhookPayload struct {
+	AppId            int    `json:"appId"`
+	EventId          int    `json:"eventId"`
+	SubscriptionId   int    `json:"subscriptionId"`
+	PortalId         int    `json:"portalId"`
+	OccurredAt       int64  `json:"occurredAt"`
+	SubscriptionType string `json:"subscriptionType"`
+	AttemptNumber    int    `json:"attemptNumber"`
+	ObjectId         int    `json:"objectId"`
+	ChangeSource     string `json:"changeSource"`
+	PropertyName     string `json:"propertyName"`
+	PropertyValue    string `json:"propertyValue"`
+	IsSensitive      bool   `json:"isSensitive"`
+}
